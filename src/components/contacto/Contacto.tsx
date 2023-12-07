f85abf57d8138c4d58ea7fc8fc9e0aa6ae1976e4
NehemiasBalsas <brandonbalsas7@gmail.com>
2023-12-07 14:23:25 -0300

    actualizado

--- src/components/contacto/Contacto.tsx
+++ src/components/contacto/Contacto.tsx
@@ -3,7 +3,8 @@ import './Contacto.css'
 import Image from 'next/image'
 import instagram from '@/assets/img/instagram (2).png';
 import linkedin from '@/assets/img/linkedin (3).png';
-import email from '@/assets/img/email.png'
+import email from '@/assets/img/email.png';
+import phone from '@/assets/img/ring-phone.png';
 import Link from 'next/link';
 
 
@@ -14,23 +15,31 @@ export default function Contacto() {
           <h3 className='title_contacto'>Contacto</h3>
           </div>
           
-          <section className='icon_contactos'>
+          <div className='contenedor_titleR'>
+          <h6 className='RedessSo'>Redes Sociales</h6>
+          </div>
+        <section className='icon_contactos'>
               
             <Link className='divC' href={'https://www.instagram.com/nehemias.balsas/'} target='_blank'>
               <Image className='redes' src={ instagram } alt='foto no disponible' />
-              <p>@nehemias.balsas</p>  
             </Link>
             <Link className='divC' href={'https://www.linkedin.com/in/brandon-nehemias-balsas-005a32206/'} target='_blank'>
-              <Image className='redes' src={ linkedin } alt='foto no disponible' />
-              <p>Brandon Nehemias Balsas</p>  
+              <Image className='redes' src={ linkedin } alt='foto no disponible' /> 
             </Link>
             <Link className='divC' href={'mailto:Brandonbalsas7@gmail.com'} target='_blank'>
-              <Image className='redes' src={ email } alt='foto no disponible' />
-              <p>brandonbalsas7@gmail.com</p>  
-            </Link>
-            
-              
-          </section>
+              <Image className='redes' src={ email } alt='foto no disponible' />  
+            </Link>  
+      </section>
+      
+      <div className='contenedor_titleR'>
+          <h6 className='RedessSo'>Telefono</h6>
+      </div>
+      <div className='telefono'>
+      <Image className='phone' src={phone} alt='foto no disponible' />
+        <p>223 698-7228</p>
+      </div>
+      
+      <p className='MarcaAgua'>Creado por Brandon Balsas ©</p>
     </div>
   )
 }
